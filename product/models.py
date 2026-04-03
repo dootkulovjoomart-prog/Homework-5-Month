58c86a84a73e20d07323c961a02b87816e072fc2
[200~ <dootkulovjoomart@gmail.com>
2026-04-03 20:10:45 +0600

    lasthw

--- product/models.py
+++ product/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+
+
     def __str__(self):
         return f'{self.name}'
 
@@ -11,14 +13,33 @@ class Product(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     price = models.IntegerField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE , null = True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE , null = True , related_name='products')
+
+    def category_list(self):
+        return  self.category.name if self.category else None
+   
+   
+   
+    def rating(self):
+        reviews = self.reviews.all()
+        count=self.reviews.all().count()
+        if not reviews :
+            return 0
+        total = 0
+        for i in reviews:
+            total += i.stars
+        return total /count
 
     def __str__(self):
         return f'{self.title}'
-
+    
 class Review(models.Model):
     text = models.TextField()
-    product = models.ForeignKey(Product , on_delete=models.CASCADE , null = True)
+    product = models.ForeignKey(Product , on_delete=models.CASCADE , null = True , related_name='reviews')
+    stars = models.IntegerField( choices=[(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5')] )
+
+
+
 
     def __str__(self):
         return f'{self.product}'
--- product/serialazers.py
+++ product/serialazers.py
@@ -2,19 +2,25 @@ from rest_framework import serializers
 from .models import Category , Product , Review 
  
 class CategoryListSerializer(serializers.ModelSerializer):
+    products = serializers.SerializerMethodField()
     class Meta:
         model = Category
-        fields = 'id name'.split()
+        fields = 'id name  products '.split()
 
+    def get_products(self,obj):
+        return obj.products.count()
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta :
         model = Product
-        fields = 'id title description price category'.split()
-
+        fields = 'id title description price category reviews category_list rating'.split()
+        # depth = 1
+    def get_rating(self , product ):
+        return product.rating()
+    
 class ReviewListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = 'text product'.split()
+        fields = 'text product stars'.split()
 
 
 class CategoryDetailSerializer(serializers.ModelSerializer):

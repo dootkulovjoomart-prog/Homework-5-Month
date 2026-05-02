ac3154ddb3e2553b763aa14f939fb3fe40a2642c
[200~ <dootkulovjoomart@gmail.com>
2026-05-02 12:12:20 +0600

    hw2

--- common/permissions.py
+++ common/permissions.py
@@ -26,9 +26,15 @@ class IsModerator(BasePermission):
     def has_permission(self, request, view): # type: ignore
         if request.user.is_superuser:
             return True
-        if request.method == "POST" and request.user.is_staff:
-            return False
-        return True
+        if request.user.is_staff:
+            if request.method == 'POST':
+                return False
+            return True
+        
+        return request.method in SAFE_METHODS
     
     def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser or request.user.is_staff:
+            return True
+        
         return obj.director == request.user
